dd255d358c94e50ee8465a7a04b58e529ca9a752
Denis Polygalov <dpolyg@gmail.com>
2019-09-06 10:03:57 +0900

    Move all *PreviewWindow() classes into common/preview.py. Share them bewteen msCap.py and msView.py

--- mstools/msView.py
+++ mstools/msView.py
@@ -14,10 +14,9 @@ from PyQt5.QtMultimedia import QCameraInfo
 
 import cv2 as cv
 
-from common.widgets import CLabeledComboBox
-from common.widgets import CLabeledSpinSlider
 from common.preview import CQCameraPreviewWindow
 from common.preview import COpenCVPreviewWindow
+from common.preview import CMiniScopePreviewWindow
 from common.capture import COpenCVframeCaptureThread
 
 
@@ -131,105 +130,6 @@ class CMainWindow(QtWidgets.QWidget):
         self.__interrupt_threads_gracefully()
         if self.win_preview != None:
             self.win_preview.close()
-#
-
-
-class CMiniScopePreviewWindow(COpenCVPreviewWindow):
-    def __init__(self, b_emulation_mode=False, *args, **kwargs):
-        super(CMiniScopePreviewWindow, self).__init__(*args, **kwargs)
-
-        self._DEVICE_ID = 0x12
-        self._RECORD_START = 0x01
-        self._RECORD_END = 0x02
-        self._TRIG_RECORD_EXT = 0x02
-        self._SET_CMOS_SETTINGS = 0x03
-        self._INIT_FRATE_VAL = 20 # in Hz
-        self._INIT_FRATE_IDX = 3 # (20 Hz)
-        self.t_frate_names = ("5 Hz", "10 Hz", "15 Hz", "20 Hz", "30 Hz", "60 Hz")
-        self.t_frate_values = (0x11, 0x12, 0x13, 0x14, 0x15, 0x16)
-        self._INIT_EXPOSURE = 255
-        self._INIT_GAIN = 16
-        self._INIT_EXCITATION = 0
-
-        self.b_emulation_mode = b_emulation_mode
-        if self.b_emulation_mode:
-            self.setWindowTitle("Miniscope (EMULATION)")
-        else:
-            self.setWindowTitle("Miniscope")
-
-        self.toolbar = QtWidgets.QToolBar("Preview")
-
-        self.btn_set_cmos = QtWidgets.QPushButton("RESET")
-        self.btn_set_cmos.clicked.connect(self.__cb_on_set_CMOS_btn_clicked)
-        self.btn_set_cmos.setSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Expanding)
-        self.toolbar.addWidget(self.btn_set_cmos)
-        self.toolbar.addSeparator()
-
-        self.cbox_frame_rate = CLabeledComboBox("Frame Rate:")
-        self.cbox_frame_rate.cbox.addItems(self.t_frate_names)
-        self.cbox_frame_rate.cbox.setCurrentIndex(self._INIT_FRATE_IDX)
-        self.cbox_frame_rate.cbox.currentIndexChanged.connect(self.__cb_on_frame_rate_cbox_index_changed)
-        self.toolbar.addWidget(self.cbox_frame_rate)
-        self.toolbar.addSeparator()
-
-        self.sld_exposure = CLabeledSpinSlider("Exposure:", (1, 255), 1, cb_action=self.__cb_on_exposure_changed)
-        self.toolbar.addWidget(self.sld_exposure)
-        self.toolbar.addSeparator()
-
-        self.sld_gain = CLabeledSpinSlider("Gain:", (16, 64), 1, cb_action=self.__cb_on_gain_changed)
-        self.toolbar.addWidget(self.sld_gain)
-        self.toolbar.addSeparator()
-
-        self.sld_excitation = CLabeledSpinSlider("Excitation:", (0, 100), 1, cb_action=self.__cb_on_excitation_changed)
-        self.toolbar.addWidget(self.sld_excitation)
-        self.toolbar.addSeparator()
-
-        # top side tool-bar
-        self.addToolBar(self.toolbar)
-
-    def __reset_UI(self):
-        self.cbox_frame_rate.cbox.setCurrentIndex(self._INIT_FRATE_IDX)
-        self.sld_exposure.slider.setSliderPosition(self._INIT_EXPOSURE)
-        self.sld_gain.slider.setSliderPosition(self._INIT_GAIN)
-        self.sld_excitation.slider.setSliderPosition(self._INIT_EXCITATION)
-        # TODO add here (re)initialization code for other GUI elements
-        # WARNING: DO NOT call self.update_cap_prop() here. Such calls must be
-        # implemented in the correspondent event handlers (i.e. self.__cb_on_*)
-
-    def __cb_on_set_CMOS_btn_clicked(self, event):
-        if not self.is_started(): return
-        self.update_cap_prop(cv.CAP_PROP_SATURATION, self._SET_CMOS_SETTINGS)
-        self.__reset_UI()
-
-    def __cb_on_frame_rate_cbox_index_changed(self, event):
-        if not self.is_started(): return
-        self.update_cap_prop(cv.CAP_PROP_SATURATION, self.t_frate_values[self.cbox_frame_rate.cbox.currentIndex()])
-
-    def __cb_on_exposure_changed(self, i_new_value):
-        self.update_cap_prop(cv.CAP_PROP_BRIGHTNESS, i_new_value)
-
-    def __cb_on_gain_changed(self, i_new_value):
-        # Gains between 32 and 64 must be even for MT9V032
-        if i_new_value >= 32 and (i_new_value % 2 == 1):
-            self.update_cap_prop(cv.CAP_PROP_GAIN, i_new_value + 1)
-        else:
-            self.update_cap_prop(cv.CAP_PROP_GAIN, i_new_value)
-
-    def __cb_on_excitation_changed(self, i_new_value):
-        i_val = int(i_new_value*(0x0FFF)/100)|0x3000
-        self.update_cap_prop(cv.CAP_PROP_HUE, (i_val>>4) & 0x00FF)
-
-    def start_preview(self, i_camera_idx, oc_camera_info, oc_frame_cap_thread):
-        super().start_preview(i_camera_idx, oc_camera_info, oc_frame_cap_thread)
-
-        # Seems like Miniscope need the initial FPS to be set too,
-        # in a way not consistent with it's further changes!
-        f_cam_fps = self.get_cap_prop(cv.CAP_PROP_FPS)
-        if abs(f_cam_fps - self._INIT_FRATE_VAL) > 0.5:
-            self.update_cap_prop(cv.CAP_PROP_FPS, self._INIT_FRATE_VAL)
-
-        self.__reset_UI()
-    #
 #
 
 
