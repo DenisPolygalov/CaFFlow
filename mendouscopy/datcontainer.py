736f53daaf6dff80f54857612edf8ce6a21c05d3
Denis Polygalov <dpolyg@gmail.com>
2020-11-10 13:18:24 +0900

    Adapt CDatContainer class to new recording session format

--- mendouscopy/datcontainer.py
+++ mendouscopy/datcontainer.py
@@ -3,11 +3,12 @@
 
 import os
 import sys
+import json
 import numpy as np
 
 
 """
-Copyright (C) 2018 Lilia Evgeniou,
+Copyright (C) 2018-2020 Lilia Evgeniou, Denis Polygalov,
 Laboratory for Circuit and Behavioral Physiology,
 RIKEN Center for Brain Science, Saitama, Japan.
 
@@ -18,7 +19,7 @@ the Free Software Foundation; either version 3 of the License, or
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
@@ -30,12 +31,62 @@ http://www.fsf.org/
 class CDatContainer(object):
     def __init__(self, s_target_dir):
         self.s_target_dir = s_target_dir
+        self.d_DAT = {}
+        self.d_DAT['target_dir'] = self.s_target_dir
+        self.d_DAT['camNum'] = []
+        self.d_DAT['frameNum'] = []
+        self.d_DAT['sysClock'] = []
+        self.d_DAT['buffer'] = []
 
-        # name the files
-        s_sett_fname = os.path.join(s_target_dir, "settings_and_notes.dat")
-        s_ts_fname   = os.path.join(s_target_dir, "timestamp.dat")
+        s_sett_fname = os.path.join(self.s_target_dir, "settings_and_notes.dat")
+        if os.path.isfile(s_sett_fname):
+            self.d_DAT.update(self.__load_settings_and_notes(s_sett_fname))
+        else:
+            s_sett_fname = os.path.join(self.s_target_dir, "rec_info.json")
+            if os.path.isfile(s_sett_fname):
+                self.d_DAT.update(self.__load_rec_info(s_sett_fname))
+            else:
+                raise ValueError("Unable to find any settings or recording information file")
 
+        # parsing timestamp.dat file
+        s_ts_fname = os.path.join(self.s_target_dir, "timestamp.dat")
+
+        with open(file=s_ts_fname, mode="r") as h_files:
+
+            for idx, s_line in enumerate(h_files):
+                s_line = s_line.strip()
+                if s_line == '': continue
+
+                # check that all lines are 4 tokens separated by tabs
+                l_tokens = s_line.split('\t')
+                if len(l_tokens) != 4:
+                    raise ValueError("Unexpected file format in file %s at line %s: %s" % (s_ts_fname, idx, s_line))
+
+                # check that first line is:
+                # "camNum\tframeNum\tsysClock\tbuffer\n"
+                if idx == 0:
+                    if s_line != "camNum\tframeNum\tsysClock\tbuffer":
+                        raise ValueError("Unexpected file format in file %s at line 0: %s" % (s_ts_fname, s_line))
+                    else:
+                        continue  # We know that this is the correct file, with the correct column names
+
+                # check that after the first line, all lines are 4 integers and add them to appropriate lists
+                try:
+                    self.d_DAT['camNum'].append(int(l_tokens[0]))
+                    self.d_DAT['frameNum'].append(int(l_tokens[1]))
+                    self.d_DAT['sysClock'].append(int(l_tokens[2]))
+                    self.d_DAT['buffer'].append(int(l_tokens[3]))
+                except ValueError:
+                    raise ValueError("Unexpected file format in file %s at line %s: %s" % (s_ts_fname, idx, s_line))
+
+        self.d_DAT['camNum']   = np.array(self.d_DAT['camNum'],   dtype=np.int64)
+        self.d_DAT['frameNum'] = np.array(self.d_DAT['frameNum'], dtype=np.int64)
+        self.d_DAT['sysClock'] = np.array(self.d_DAT['sysClock'], dtype=np.int64)
+        self.d_DAT['buffer']   = np.array(self.d_DAT['buffer'],   dtype=np.int64)
+
+    def __load_settings_and_notes(self, s_sett_fname):
         # parsing settings_and_notes.dat file
+        # this method will either return valid dictionary or raise exception
 
         # make a list of lists: list of "\t"-separated strings in a line; 4 lines in larger list
         with open(s_sett_fname) as h_file:
@@ -57,7 +108,7 @@ class CDatContainer(object):
         l_param_items = [int(item) if item.isnumeric() else item for item in l_settings_and_notes[1]]
 
         # dictionary with l_param_keys as keys corresponding to the l_param_items as the items (with same index)
-        self.d_DAT = {key:l_param_items[item] for item, key in enumerate(l_param_keys)}
+        d_sett = {key:l_param_items[item] for item, key in enumerate(l_param_keys)}
 
         # keys of d_behav_ROI
         l_behav_ROI_keys = ["bhv_roi_x", "bhv_roi_y", "bhv_roi_w", "bhv_roi_h"]
@@ -66,119 +117,21 @@ class CDatContainer(object):
 
         # dictionary with l_behav_ROI_keys as keys corresponding to the l_behav_ROI_items as the items (with same index)
         d_behav_ROI = {key: l_behav_ROI_items[item] for item, key in enumerate(l_behav_ROI_keys)}
-        self.d_DAT.update(d_behav_ROI)
-        self.d_DAT['target_dir'] = self.s_target_dir
+        d_sett.update(d_behav_ROI)
+        return d_sett
 
-        # parsing timestamp.dat file
-
-        # initialize lists for each column
-        self.l_camNum = []
-        self.l_frameNum = []
-        self.l_sysClock = []
-        self.l_buffer = []
-
-        self.d_DAT['camNum'] = []
-        self.d_DAT['frameNum'] = []
-        self.d_DAT['sysClock'] = []
-        self.d_DAT['buffer'] = []
-
-        self.d_DAT['frameNum_cam0'] = []
-        self.d_DAT['sysClock_cam0'] = []
-        self.d_DAT['buffer_cam0'] = []
-
-        self.d_DAT['frameNum_cam1'] = []
-        self.d_DAT['sysClock_cam1'] = []
-        self.d_DAT['buffer_cam1'] = []
-
-        with open(file=s_ts_fname, mode="r") as h_files:
-
-            for idx, s_line in enumerate(h_files):
-
-                s_line = s_line.strip()
-
-                if s_line == '': continue
-
-                # check that all lines are 4 tokens separated by tabs
-                l_tokens = s_line.split('\t')
-                if len(l_tokens) != 4:
-                    raise ValueError("Unexpected file format in file %s at line %s: %s" % (s_ts_fname, idx, s_line))
-
-                # check that first line is:
-                # "camNum\tframeNum\tsysClock\tbuffer\n"
-                if idx == 0:
-                    if s_line != "camNum\tframeNum\tsysClock\tbuffer":
-                        raise ValueError("Unexpected file format in file %s at line 0: %s" % (s_ts_fname, s_line))
-                    else:
-                        continue  # We know that this is the correct file, with the correct column names
-
-                # check that after the first line, all lines are 4 integers and add them to appropriate lists
-                try:
-                    self.l_camNum.append(int(l_tokens[0]))
-                    self.l_frameNum.append(int(l_tokens[1]))
-                    self.l_sysClock.append(int(l_tokens[2]))
-                    self.l_buffer.append(int(l_tokens[3]))
-
-                    self.d_DAT['camNum'].append(int(l_tokens[0]))
-                    self.d_DAT['frameNum'].append(int(l_tokens[1]))
-                    self.d_DAT['sysClock'].append(int(l_tokens[2]))
-                    self.d_DAT['buffer'].append(int(l_tokens[3]))
-                except ValueError:
-                    raise ValueError("Unexpected file format in file %s at line %s: %s" % (s_ts_fname, idx, s_line))
-
-            # check if the self.l_camNum list contain only zeros and ones (regardless of order)
-            #na_camNum = np.array(self.l_camNum, np.int)
-            #if not ((0 <= na_camNum).all() and (na_camNum <= 1).all()):
-            #    raise ValueError("Unexpected file format: number other than 0 or 1 found in camNum")
-
-            # check if the self.l_camNum list is 101010... OR 010101...
-            # THIS WILL NOT WORK BECAUSE THERE ARE MANY CASES OF WRONG ALTERNATION
-            #if (na_camNum[::2] == 0).all() and (na_camNum[1::2] == 1).all():  # 01010101...
-            #    b_status = True  # so that I know later that it is in the "even" configuration
-            #elif (na_camNum[::2] == 1).all() and (na_camNum[1::2] == 0).all():  # 10101010...
-            #    b_status = False  # "odd" configuration
-            #else:
-            #    raise ValueError("Unexpected file format")  # give more information
-
-            # split 3 lists into two categories according to the camera number
-            self.l_frameNum_cam0 = []
-            self.l_sysClock_cam0 = []
-            self.l_buffer_cam0 = []
-            self.l_frameNum_cam1 = []
-            self.l_sysClock_cam1 = []
-            self.l_buffer_cam1 = []
-
-            for i in range(len(self.l_camNum)):
-                if self.l_camNum[i] == 0:
-                    self.l_frameNum_cam0.append(self.l_frameNum[i])
-                    self.l_sysClock_cam0.append(self.l_sysClock[i])
-                    self.l_buffer_cam0.append(self.l_buffer[i])
-
-                    self.d_DAT['frameNum_cam0'].append(self.l_frameNum[i])
-                    self.d_DAT['sysClock_cam0'].append(self.l_sysClock[i])
-                    self.d_DAT['buffer_cam0'].append(self.l_buffer[i])
-                elif self.l_camNum[i] == 1:
-                    self.l_frameNum_cam1.append(self.l_frameNum[i])
-                    self.l_sysClock_cam1.append(self.l_sysClock[i])
-                    self.l_buffer_cam1.append(self.l_buffer[i])
-
-                    self.d_DAT['frameNum_cam1'].append(self.l_frameNum[i])
-                    self.d_DAT['sysClock_cam1'].append(self.l_sysClock[i])
-                    self.d_DAT['buffer_cam1'].append(self.l_buffer[i])
-                else:
-                    raise ValueError("Unexpected file format: number other than 0 or 1 found in camNum: %i" % self.l_camNum[i])
-
-        self.d_DAT['camNum']   = np.array(self.d_DAT['camNum'],   dtype=np.int64)
-        self.d_DAT['frameNum'] = np.array(self.d_DAT['frameNum'], dtype=np.int64)
-        self.d_DAT['sysClock'] = np.array(self.d_DAT['sysClock'], dtype=np.int64)
-        self.d_DAT['buffer']   = np.array(self.d_DAT['buffer'],   dtype=np.int64)
-
-        self.d_DAT['frameNum_cam0'] = np.array(self.d_DAT['frameNum_cam0'], dtype=np.int64)
-        self.d_DAT['sysClock_cam0'] = np.array(self.d_DAT['sysClock_cam0'], dtype=np.int64)
-        self.d_DAT['buffer_cam0']   = np.array(self.d_DAT['buffer_cam0'],   dtype=np.int64)
-
-        self.d_DAT['frameNum_cam1'] = np.array(self.d_DAT['frameNum_cam1'], dtype=np.int64)
-        self.d_DAT['sysClock_cam1'] = np.array(self.d_DAT['sysClock_cam1'], dtype=np.int64)
-        self.d_DAT['buffer_cam1']   = np.array(self.d_DAT['buffer_cam1'],   dtype=np.int64)
+    def __load_rec_info(self, s_sett_fname):
+        # parsing rec_info.json file
+        # this method will either return valid dictionary or raise exception
+        with open(s_sett_fname) as json_file:
+            data = json.load(json_file)
+            if not isinstance(data, dict):
+                raise ValueError("Unexpected file format: %s" % s_sett_fname)
+            for i_idx, vstream in enumerate(data['VSTREAM_LIST']):
+                if vstream is None:
+                    data['VSTREAM_LIST'][i_idx] = np.nan
+            return data
+#
 
 if __name__ == '__main__':
     # check if argument passed by user
