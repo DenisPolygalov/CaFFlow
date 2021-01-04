be768170424b9aed8a935ab2aaa63a0fd1c87fa6
Denis Polygalov <dpolyg@gmail.com>
2021-01-04 22:19:27 +0900

    Removed trailing spaces

--- mendouscopy/sliding.py
+++ mendouscopy/sliding.py
@@ -112,35 +112,35 @@ def array_for_sliding_window(x, wshape):
     Build a sliding-window representation of x.
 
     The last dimension(s) of the output array contain the data of
-    the specific window.  The number of dimensions in the output is 
+    the specific window. The number of dimensions in the output is
     twice that of the input.
 
     Parameters
     ----------
     x : ndarray_like
-       An array for which is desired a representation to which sliding-windows 
+       An array for which is desired a representation to which sliding-windows
        computations can be easily applied.
     wshape : int or tuple
-       If an integer, then it is converted into a tuple of size given by the 
+       If an integer, then it is converted into a tuple of size given by the
        number of dimensions of x with every element set to that integer.
        If a tuple, then it should be the shape of the desired window-function
 
     Returns
     -------
     out : ndarray
-        Return a zero-copy view of the data in x so that operations can be 
-        performed over the last dimensions of this new array and be equivalent 
-        to a sliding window calculation.  The shape of out is 2*x.ndim with 
-        the shape of the last nd dimensions equal to wshape while the shape 
+        Return a zero-copy view of the data in x so that operations can be
+        performed over the last dimensions of this new array and be equivalent
+        to a sliding window calculation.  The shape of out is 2*x.ndim with
+        the shape of the last nd dimensions equal to wshape while the shape
         of the first n dimensions is found by subtracting the window shape
-        from the input shape and adding one in each dimension.  This is 
+        from the input shape and adding one in each dimension. This is
         the number of "complete" blocks of shape wshape in x.
 
     Raises
     ------
     ValueError
         If the size of wshape is not x.ndim (unless wshape is an integer).
-        If one of the dimensions of wshape exceeds the input array. 
+        If one of the dimensions of wshape exceeds the input array.
 
     Examples
     --------
@@ -152,7 +152,7 @@ def array_for_sliding_window(x, wshape):
     array([[ 1.,  2.,  3.],
            [ 2.,  3.,  4.],
            [ 3.,  4.,  5.]])
-           
+
     >>> x = np.arange(1,17).reshape(4,4)
     >>> x
     array([[ 1,  2,  3,  4],
